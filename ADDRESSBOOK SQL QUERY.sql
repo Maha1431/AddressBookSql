391ebed95eec3dc9653e9c44714ff7497ec72af7
maha <mmahaece07@gmail.com>
2022-01-11 11:16:11 +0530

    Sql Queries upto UC13

--- ADDRESSBOOK SQL QUERY.sql
+++ ADDRESSBOOK SQL QUERY.sql
@@ -1,8 +1,7 @@
-//uc1
 create database AddressBookservice;
-show databases;
-use Addressbook_service;
-//uc2
+--UC1
+use Addressbookservice;
+--UC2
 create table Address_Book(
 FirstName varchar(30) not null,
 LastName varchar(20) not null,
@@ -13,55 +12,44 @@ Zipcode varchar(30) not null,
 PhoneNumber varchar(20) not null,
 Email varchar(50) not null
 );
-describe Address_book;
-select * from Address_book;
-//uc3
+--UC3
 insert into Address_book Values
 ('maha', 'lakshmi', 'thyagarajanagar', 'banglore', 'karnataka', '8787', '7783727255', 'klal@gmail.com'),
 ('teju', 'hasini', 'shastri nagar', 'pune', 'mumbai', '9898', '9277277621', 'jjaha@gmail.com'),
 ('promod', 'jain', 'banshankari', 'chennai', 'Tamil nadu', '2872', '7727777271', 'jsh@gmail.com');
-
-select *from Address_Book;
-
-//uc4
+--UC4
+SELECT * FROM Address_Book;
 update Address_Book set Zipcode = '1919', Email = 'mmahaece072gmail.com' where Firstname = 'maha';
 select *from Address_Book;
-describe Address_Book;
-// uc5
+--UC5
 delete  from Address_Book
   where FirstName = 'teju';	
   select *from Address_Book;
-  
- //uc6
-  select * from Address_Book
-  where city = 'chennai' or state = 'Tamil nadu';
-  
-  //uc7
-  select City,count(City) from Address_Book group by City;
-  select State,count(State) from Address_Book group by State;
-  
-  //uc8
-  select * from Address_Book
+  --UC6
+select * from Address_Book
+where city = 'chennai' or state = 'Tamil nadu';
+  ---UC7
+select City,count(City) from Address_Book group by City; select State,count(State) from Address_Book group by State;
+  --UC8
+ select * from Address_Book
 where City = 'banglore'
 order by FirstName asc;
 
-//uc9
+INSERT INTO Address_Book VALUES('raju', 'hasini', 'shastri nagar', 'pune', 'mumbai', '9898', '9277277621', 'jjaha@gmail.com');
+--UC9
 Alter table Address_Book add Addressbook_name varchar(25),type varchar(50)
 update Address_Book set Addressbook_name = 'Home',type = 'Family' where FirstName = 'maha' 
 update Address_Book set Addressbook_name = 'Home',type = 'Friends' where FirstName = 'promod' 
-update Address_Book set Addressbook_name = 'Office',type = 'Profession' where FirstName = 'teju';
+--update Address_Book set Addressbook_name = 'Office',type = 'Profession' where FirstName = 'teju';
 select * from Address_Book;
-//uc10
-
+--UC10
  select type, COUNT(FirstName) from address_book group by type;
- //uc11
- create table Addressbook(
+ --UC11
+  create table Addressbook(
  AddressBookID int identity(1,1) primary key,
 AddressBookName varchar(100)
-)
-
-Insert into Addressbook values ('Family'),('Freinds')
-
+);
+Insert into Addressbook values ('Family'),('Freinds');
 select * from Addressbook;
 
 create table Contact_Person(
@@ -75,7 +63,8 @@ StateName varchar(100),
 ZipCode BigInt,
 PhoneNum BigInt,
 EmailId varchar(200),
-foreign key (AddressBook_ID) references AddressBook(AddressBookID))
+foreign key (AddressBook_ID) references AddressBook(AddressBookID));
+
 
 Insert into Contact_Person values
 (1,'raju','karan','Mailapur','chennai','tamilnadu',38737,28388328,'r.k@gmail.com'),
@@ -83,13 +72,13 @@ Insert into Contact_Person values
 (2,'promod','Kumar','T.R nagar','hydrabad','andhra pradesh',098988,017211551,'kumar@gmail.com'),
 (1,'shalu','malini','h.s.r layout','nellore','tamilnadu',76263,8282718711,'shalu@gmail.com');
 
-select * from Contact_Person
+select * from Contact_Person;
 
 ---Create contact_type table
 create table Contact_Type
 (ContactTypeID int identity(1,1) primary key,
 ContactTypeName varchar(100)
-)
+);
 
 ---Insert the values in contat_type
 Insert into Contact_Type values
@@ -104,7 +93,7 @@ ContactType_ID int,
 Contact_ID int,
 foreign key (ContactType_ID) references Contact_Type(ContactTypeID),
 foreign key (Contact_ID) references Contact_Person(ContactID)
-)
+);
 
 ---Insert the values in relation_type
 insert into Relation_Type values
@@ -116,21 +105,21 @@ insert into Relation_Type values
 --Retrieve the data
 Select * from Relation_Type;
 
-//UC12
+--UC12
 select AddressBookName,FirstName,LastName,Address,City,StateName,ZipCode,PhoneNum,EmailId,ContactTypeName
 from Addressbook 
 Full JOIN Contact_Person on Addressbook.AddressBookID=Contact_Person.AddressBook_ID 
 Full JOIN Relation_Type on Relation_Type.Contact_ID=Contact_Person.ContactID
 Full JOIN Contact_Type on Relation_Type.ContactType_ID=Contact_Type.ContactTypeID
 
-//Uc13
-//uc6
+--Uc13
+--UC6
 select AddressBookName,FirstName,LastName,Address,City,StateName,ZipCode,PhoneNum,EmailId,ContactTypeName
 from Contact_Person 
 INNER JOIN  Addressbook on Addressbook.AddressBookID=Contact_Person.AddressBook_ID and (City='Chennai' or StateName='tamilNadu')
 INNER JOIN Relation_Type on Relation_Type.Contact_ID=Contact_Person.ContactID
 INNER JOIN Contact_Type on Relation_Type.ContactType_ID=Contact_Type.ContactTypeID
-//UC7
+--UC7
 select Count(*)As CountOfStateAndCity ,StateName,City
 from Contact_Person 
 INNER JOIN  Addressbook on Addressbook.AddressBookID=AddressBook_ID 
@@ -151,9 +140,3 @@ INNER JOIN Contact_Type on Relation_Type.ContactType_ID=Contact_Type.ContactType
 Group by ContactTypeName
 
 select * from Address_Book
-
-
-  
-  
-
-  
